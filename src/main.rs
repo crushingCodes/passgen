f80f497e35706d45858102ebb98c3f5139985aeb
Gavin <26400289+crushingCodes@users.noreply.github.com>
2025-03-26 17:36:34 +0900

    add clipboard functionality

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
-use std::io;
+use cli_clipboard::{ClipboardContext, ClipboardProvider};
+// use std::io;
+
 fn main() {
+    let mut ctx = ClipboardContext::new().unwrap();
     let password = passgen::generate_password().unwrap();
-    let mut name = String::new();
-    println!("> Enter a name:");
-    io::stdin()
-        .read_line(&mut name)
-        .expect("Failed to read name");
-    println!("{}", password);
+    ctx.set_contents(password.to_owned()).unwrap();
+    println!("Password copied to clipboard!");
+
+    //TODO:
+    // let mut name = String::new();
+    // println!("> Enter a name:");
+    // io::stdin()
+    //     .read_line(&mut name)
+    //     .expect("Failed to read name");
+    // println!("{}", password);
 }
